a332fc10bf292fed5e03d5dcaf11560da58fcf81
Matías Concha <matiascoar@gmail.com>
2020-12-27 21:36:17 -0300

    Añadiendo archivos del proyecto.

---
+++ main.py
@@ -0,0 +1,143 @@
+#packages for mask detection
+import numpy as np
+from cv2 import cv2
+import tensorflow as tf
+from object_detection.utils import label_map_util
+from object_detection.utils import config_util
+from object_detection.utils import visualization_utils as viz_utils
+from object_detection.builders import model_builder
+from playsound import playsound
+
+#packages for the web server
+from flask import Flask, render_template, Response
+
+def detect(image, detection_model):
+    image, shapes = detection_model.preprocess(image)
+    prediction_dict = detection_model.predict(image, shapes)
+    detections = detection_model.postprocess(prediction_dict, shapes)
+
+    return detections, prediction_dict, tf.reshape(shapes, [-1])
+
+
+def run(label_map_path, config_file_path, checkpoint_path):
+
+    # Enable GPU dynamic memory allocation
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    for gpu in gpus:
+        tf.config.experimental.set_memory_growth(gpu, True)
+
+    # Load pipeline config and build a detection model
+    configs = config_util.get_configs_from_pipeline_file(config_file_path)
+    model_config = configs['model']
+    detection_model = model_builder.build(model_config=model_config, is_training=False)
+
+    # Restore checkpoint
+    ckpt = tf.compat.v2.train.Checkpoint(model=detection_model)
+    ckpt.restore(checkpoint_path).expect_partial()
+
+    category_index = label_map_util.create_category_index_from_labelmap(label_map_path, use_display_name=True)
+
+    videoStreamAddress = "http://192.168.1.82:4747/video"
+
+    cap = cv2.VideoCapture(videoStreamAddress) #esto básicamente
+    
+    while True:
+        # Read frame from camera
+        ret, image_np = cap.read()
+
+        input_tensor = tf.convert_to_tensor(np.expand_dims(image_np, 0), dtype=tf.float32)
+        detections, predictions_dict, shapes = detect(input_tensor, detection_model)
+
+        detection_boxes = detections['detection_boxes'][0].numpy()
+        detection_classes = detections['detection_classes'][0].numpy()
+        detection_scores = detections['detection_scores'][0].numpy()
+
+        indexes = np.array(tf.image.non_max_suppression(
+            detection_boxes,
+            detection_scores,
+            max_output_size=100,
+            iou_threshold=0.5,
+            score_threshold=0.3))
+
+        detection_boxes = detection_boxes[indexes]
+        detection_classes = detection_classes[indexes]
+        detection_scores = detection_scores[indexes]
+
+        label_id_offset = 1
+        image_np_with_detections = image_np.copy()
+        bb_color = 'green'
+
+
+        if indexes.shape != 0:
+            viz_utils.visualize_boxes_and_labels_on_image_array(
+                image_np_with_detections,
+                detection_boxes,
+                (detection_classes + label_id_offset).astype(int),
+                detection_scores,
+                category_index,
+                use_normalized_coordinates=True,
+                max_boxes_to_draw=10,
+                min_score_thresh=.30,
+                agnostic_mode=False,
+                semaphore_mode=True)
+
+            # si cualquiera de las personas frente a la cámara está usando mal la mascarilla o no tiene, marcar recuadro rojo
+            if any(c == 1 or c == 3 for c in detection_classes):
+                bb_color = 'green' #rojo
+            # sino, todo ok, verde
+            else:
+                bb_color = 'blue' #verde
+            
+            viz_utils.draw_bounding_box_on_image_array(
+                image_np_with_detections,
+                0, 0, 1, 1, 
+                color = bb_color,
+                thickness = 20)
+
+        print(detection_boxes, detection_classes, detection_scores)
+        print((detection_classes + label_id_offset).astype(int))
+        
+        if any(c == 1 or c == 3 for c in detection_classes):
+           playsound('templates/alarm.mp3')
+
+        frame = cv2.resize(image_np_with_detections, (800, 600))
+
+        # Display output
+        #cv2.imshow('MaskON: Uso correcto de mascarilla', cv2.resize(image_np_with_detections, (800, 600)))
+        # encode OpenCV raw frame to jpg and displaying it
+        
+        ret, jpeg = cv2.imencode('.jpg', frame)
+        frame_encoded_jpeg = jpeg.tobytes()
+
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame_encoded_jpeg + b'\r\n\r\n')
+    
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+#mounting web server TODO: Searar en un módulo aparte.
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # rendering webpage
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(run(
+                        label_map_path=PATH_TO_LABELMAP,
+                        config_file_path=PATH_TO_CONFIG,
+                        checkpoint_path=PATH_TO_CHECKPOINT),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    PATH_TO_LABELMAP = r"C:\Users\matia\Documents\Scripts\PDI\training_resources\training\001\label_map.pbtxt"
+    PATH_TO_CONFIG = r"C:\Users\matia\Documents\Scripts\PDI\training_resources\training\001\pipeline.config"
+    PATH_TO_CHECKPOINT = r"C:\Users\matia\Documents\Scripts\PDI\training_resources\training\001\ckpt-17"
+
+    #run(label_map_path=PATH_TO_LABELMAP, config_file_path=PATH_TO_CONFIG, checkpoint_path=PATH_TO_CHECKPOINT)
+    app.run(host='127.0.0.1',port='5000', debug=True)
---
+++ tfrecord_converter.py
@@ -0,0 +1,181 @@
+"""
+Serialization tutorial: https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/using_your_own_dataset.md
+Serialization example of Pascal VOC dataset: https://github.com/tensorflow/models/blob/master/research/object_detection/dataset_tools/create_pascal_tf_record.py
+"""
+
+from object_detection.utils import dataset_util
+import xml.etree.ElementTree as ET
+import tensorflow as tf
+from PIL import Image
+from os import path
+import numpy as np
+import random
+import glob
+
+
+class Example:
+    class_name_dict = {}
+    class_count = 0
+
+    def __init__(self, filename, width, height):
+        self.filename = filename
+        self.height = height
+        self.width = width
+
+        self.xmins = []
+        self.xmaxs = []
+        self.ymins = []
+        self.ymaxs = []
+
+        self.class_names = []
+        self.class_ids = []
+
+    def add_xmin(self, xmin):
+        xmin_normalized = xmin / self.width
+        self.xmins.append(xmin_normalized)
+
+    def add_xmax(self, xmax):
+        xmax_normalized = xmax / self.width
+        self.xmaxs.append(xmax_normalized)
+
+    def add_ymin(self, ymin):
+        ymin_normalized = ymin / self.height
+        self.ymins.append(ymin_normalized)
+
+    def add_ymax(self, ymax):
+        ymax_normalized = ymax / self.height
+        self.ymaxs.append(ymax_normalized)
+
+    def add_class_name(self, class_name):
+        if class_name not in Example.class_name_dict:
+            Example.class_count += 1
+            Example.class_name_dict[class_name] = Example.class_count
+
+        self.class_names.append(class_name.encode('utf8'))
+        self.class_ids.append(Example.class_name_dict[class_name])
+
+    @staticmethod
+    def create_label_map(output_labelmap_path):
+        keys = Example.class_name_dict.keys()
+        label_map_string = ""
+        for key in keys:
+            id = Example.class_name_dict[key]
+            name = key
+            item_string = \
+                "item { \n" \
+                f"  id: {id}\n" \
+                f"  name: \"{name}\"\n" \
+                "}\n\n"
+            label_map_string += item_string
+
+        with open(output_labelmap_path, "w") as text_file:
+            print(label_map_string, file=text_file)
+
+
+def load_annotations(annotation_folder_path):
+    '''
+    Finds all xml files in annotation_folder_path and parse it into a Example object
+    '''
+    xml_file_paths = glob.glob(path.join(annotation_folder_path, '*.xml'))
+    examples = []
+    for xml_file_path in xml_file_paths:
+        print(f'Loading data from {xml_file_path}')
+        tree = ET.parse(xml_file_path)
+        root = tree.getroot()
+        objects = root.findall('object')
+        example = Example(
+            filename=root.find('filename').text,
+            width=int(root.find('size')[0].text),
+            height=int(root.find('size')[1].text))
+
+        for object in objects:
+            boundary_box = object.find('bndbox')
+            example.add_xmin(int(boundary_box.find('xmin').text))
+            example.add_ymin(int(boundary_box.find('ymin').text))
+            example.add_xmax(int(boundary_box.find('xmax').text))
+            example.add_ymax(int(boundary_box.find('ymax').text))
+
+            example.add_class_name(object.find('name').text)
+
+        examples.append(example)
+
+    return examples
+
+
+def create_tf_example(example, images_folder_path, img_format):
+    print(f'Creating TFRecord of file:  {example.filename}')
+    height = example.height  # Image height
+    width = example.width  # Image width
+    filename = example.filename.encode('utf8')  # Filename of the image. Empty if image is not from file
+    encoded_image_data = encode_image(full_img_path=path.join(images_folder_path, example.filename), img_format=img_format)
+
+    image_format = str.encode(img_format)
+
+    xmins = example.xmins  # List of normalized left x coordinates in bounding box (1 per box)
+    xmaxs = example.xmaxs  # List of normalized right x coordinates in bounding box (1 per box)
+    ymins = example.ymins  # List of normalized top y coordinates in bounding box (1 per box)
+    ymaxs = example.ymaxs  # List of normalized bottom y coordinates in bounding box (1 per box)
+    classes_text = example.class_names  # List of string class name of bounding box (1 per box)
+    classes = example.class_ids  # List of integer class id of bounding box (1 per box)
+
+    tf_example = tf.train.Example(features=tf.train.Features(feature={
+        'image/height': dataset_util.int64_feature(height),
+        'image/width': dataset_util.int64_feature(width),
+        'image/filename': dataset_util.bytes_feature(filename),
+        'image/source_id': dataset_util.bytes_feature(filename),
+        'image/encoded': dataset_util.bytes_feature(encoded_image_data),
+        'image/format': dataset_util.bytes_feature(image_format),
+        'image/object/bbox/xmin': dataset_util.float_list_feature(xmins),
+        'image/object/bbox/xmax': dataset_util.float_list_feature(xmaxs),
+        'image/object/bbox/ymin': dataset_util.float_list_feature(ymins),
+        'image/object/bbox/ymax': dataset_util.float_list_feature(ymaxs),
+        'image/object/class/text': dataset_util.bytes_list_feature(classes_text),
+        'image/object/class/label': dataset_util.int64_list_feature(classes),
+    }))
+    return tf_example
+
+
+def encode_image(full_img_path, img_format):
+
+    im = Image.open(full_img_path)
+    rgb_im = im.convert('RGB')
+
+    if img_format == 'png':
+        return tf.io.encode_png(np.asarray(rgb_im)).numpy()
+
+    if img_format == 'jpeg':
+        return tf.io.encode_jpeg(np.asarray(rgb_im)).numpy()
+
+
+def split_dataset(examples, train_ratio=0.8):
+    random.shuffle(examples)
+    split_index = int(len(examples) * train_ratio)
+
+    train_examples = examples[:split_index]
+    validation_examples = examples[split_index:]
+
+    return train_examples, validation_examples
+
+
+def create_tf_record_file(examples, images_folder_path, img_format, output_file_path):
+    writer = tf.compat.v1.python_io.TFRecordWriter(output_file_path)
+    for example in examples:
+        tf_example = create_tf_example(example, images_folder_path, img_format)
+        writer.write(tf_example.SerializeToString())
+    writer.close()
+
+
+if __name__ == '__main__':
+    # ↓↓↓ INPUT PARAMETERS ↓↓↓
+    annotation_folder_path = path.normpath(r"C:\Users\matia\Documents\Scripts\PDI\archive\annotations")
+    images_folder_path = path.normpath(r"C:\Users\matia\Documents\Scripts\PDI\archive\images")
+    output_folder_path = path.normpath(r"C:\Users\matia\Documents\Scripts\PDI\archive\dataset")
+    train_ratio = 0.8
+    img_format = 'jpeg'  # png or jpeg
+
+    examples = load_annotations(annotation_folder_path)
+    (train_examples, validation_examples) = split_dataset(examples=examples, train_ratio=train_ratio)
+
+    create_tf_record_file(train_examples, images_folder_path, img_format, path.join(output_folder_path, "train.record"))
+    create_tf_record_file(validation_examples, images_folder_path, img_format, path.join(output_folder_path, "eval.record"))
+    Example.create_label_map(output_labelmap_path=path.join(output_folder_path, "label_map.pbtxt"))
