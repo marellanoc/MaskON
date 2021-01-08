d49da30e8764910fab27d8bf0ae35bd5d5b82fe1
Matías Concha <matiascoar@gmail.com>
2021-01-08 00:31:42 -0300

    Entrega FINAL

--- main_web.py
+++ main_web.py
@@ -1,71 +1,89 @@
-# web server and socket
-from flask import Flask, render_template, request, jsonify, redirect, Response
+# imports de Flask
+from flask import Flask, render_template, request, jsonify, redirect
 from flask_socketio import SocketIO, emit
 from werkzeug.utils import secure_filename
+
+# imports para manejo de json y rutas dentro del disco
 import json, os
 
-#  variables y settings
-
-data = dict()
-with open('config.json') as json_file:
-    data = json.load(json_file)
-
-ALLOWED_EXTENSIONS = ['mp3']
+# carpeta donde se suben las alarmas
 UPLOAD_FOLDER = os.path.join(os.path.abspath(os.getcwd()), 'static')
 UPLOAD_FOLDER = os.path.join(UPLOAD_FOLDER, 'alarms')
 
+# diccionario que guarda las configuraciones
+data = dict()
+# se cargan las configuraciones desde el json de configuraciones
+with open('config.json') as json_file:
+    data = json.load(json_file)
+
+#creacion del server y configuracion de la carpeta de subida
 app = Flask(__name__, template_folder='templates')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-socketio = SocketIO(app, debug=True, async_mode="eventlet")
+socketio = SocketIO(app) # socket que enviará mensajes al front
 
 
+# Rutas
+
+#ruta que redirecciona de root hacia home
 @app.route('/')
+def old_index():
+    return redirect("home", code=303)
+
+#ruta principal, aqui se encuentra el stream de video
+@app.route('/home')
 def index():
-    resp = render_template("index.html")
+    resp = render_template("template.html")
     return resp
 
+# ruta que muestra la pagina de configuracion
 @app.route('/settings')
 def settings():
     resp = render_template("settings.html")
     return resp
 
+# ruta utilizada para encender y apagar la alarma en la pagina web
+@app.route('/test<n>')
+def test(n):
+    emit('alarm', {'alarm': n}, namespace='/', broadcast=True) # se avisa a la pagina por medio del socket
+    return "200"
+
+# ruta para enviar la configuracion a la figuracion
 @app.route('/get_config')
 def get_settings():
     return jsonify(data)
 
-
+# ruta utilizada para guardar las configuraciones realizadas en la pagina web
 @app.route('/post_config', methods=['POST'])
 def post_settings():
     global data
+
     if request.method == 'POST':
-        data = request.get_json()
+        data = request.get_json() # se sobreescribe la variable que guara la configuracion
+
+        # se sobreescribe el archivo de configuraciones
         with open('config.json', 'w') as fp:
             json.dump(data, fp)
+
     return '200'
 
+# ruta para guardar los tonos de alarma
 @app.route('/post_file', methods=['POST'])
 def post_file():
+
     f = request.files['file']
-    print(os.path.join(app.config['UPLOAD_FOLDER'], f.filename))
-    f.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(f.filename)))
+    f.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(f.filename))) # se utiliza secure_filename para eliminar caracteres no permitidos
+    
     return '200'
 
-@app.route('/test<n>')
-def test(n):
-    socketio.emit('alarm', {'alarm': n}, namespace='/', broadcast=True)
-    return "hola"
-
-@socketio.on('alarm_change', namespace='/')
+#listener utilizado para testear conexion con el front
+@socketio.on('alarm_change')
 def alarm_event(json):
     print(json)
-    socketio.emit('my response', json, namespace='/', broadcast=True)
+
 
 
 if __name__ == '__main__':
-    PATH_TO_LABELMAP = r"C:\Users\matia\Documents\Scripts\PDI\training_resources\training\001\label_map.pbtxt"
-    PATH_TO_CONFIG = r"C:\Users\matia\Documents\Scripts\PDI\training_resources\training\pipeline.config"
-    PATH_TO_CHECKPOINT = r"C:\Users\matia\Documents\Scripts\PDI\training_resources\training\001\ckpt-16"
-
-    #app.run(host='127.0.0.1',port='5000', debug=True)
+    # app.run(host='192.168.0.13',port='5000', debug=True)
+    #se envuelve el server en el socket y se ejecuta
     socketio.run(app)
--- static/js/settings.js
+++ static/js/settings.js
@@ -1,115 +1,108 @@
-function test() {
-    console.log('miau');
-}
-
-function check_data(){
-    console.log('haciendo get');
-    data = JSON.parse(sessionStorage.getItem('data'));
-    console.log(data);
-    if (data == null){
-        //aqui se hace el get al servidor
-        // se guarga en session storage
+function check_data(){ //funcion que chequea si existe la configuracion en la sessionStorage
+    data = JSON.parse(sessionStorage.getItem('data')); // se intenta sacar la configuracion de la sessionStorage
+    if (data == null){ //si no esta, se debe obtener
+        
         $.ajax({
-            url: "/get_config",
-            type: 'GET',
-            success: function(res) {
-                console.log(res);
-                sessionStorage.setItem('data', JSON.stringify(res))
-                data = JSON.parse(sessionStorage.getItem('data'));
-                show_config();
-            }
-        });
+        url: "/get_config", //se fija la u del servidor donde obtener la configuracion
+        type: 'GET',
+        success: function(res) { 
+            sessionStorage.setItem('data',JSON.stringify(res)) //una vez obtenida se guarda en la sessionStorage
+            data=JSON.parse(sessionStorage.getItem('data')); // se saca de la sessionStorage y se guarda en data
+            show_config() //muestra la configuracion en pantalla
+        }
+    });
     }
-    else {
+    else{ //si la informacion está en la sessionStorage se configura directamente
         show_config()
     }
 }
 
-function show_config(){
-    document.getElementById("volume").innerHTML = data['volumen_alarma'];
-    document.getElementById("tone").innerHTML = data['tono_alarma'];
+function show_config(){ //cambia los valores de los componentes html para mostrar la configuracion del servidor 
+    document.getElementById("volume").innerHTML = data['volumen_alarma']; //ajuste de volumen
+    document.getElementById("tone").innerHTML = data['tono_alarma']; // ajuste del tono de la alarma
     if(data['estado_alarma'] == 1){
-        $('#check_alarm').prop('checked',true);
+        $('#check_alarm').prop('checked',true); //se activa el slide en caso de que la alarma este activa
     }
     else if(data['estado_alarma'] == 0){
-        console.log('apagada')
-        $('#check_alarm').prop('checked',false);
+        $('#check_alarm').prop('checked',false); //se deja el slide apagado si la alarma esta desactivada
     }
 }
 
-function change_state() {
-    if (data['estado_alarma'] == 1) {
-        data['estado_alarma'] = 0
+function change_state() { //funcion que cambia el estado de la alarma
+    if (data['estado_alarma'] == 1) { //si esta activa
+        data['estado_alarma'] = 0 //se desactiva
 
-    } else if (data['estado_alarma'] == 0) {
-        data['estado_alarma'] = 1
+    } else if (data['estado_alarma'] == 0) { //si esta desactivada 
+        data['estado_alarma'] = 1 //se activa
     }
-    post_data();
+    post_data(); //se manda la configuracion al server
 }
 
-function post_data(){
-    sessionStorage.setItem('data', JSON.stringify(data))
+function post_data(){ //funcion que manda la configuracion actual al servidor
+    sessionStorage.setItem('data',JSON.stringify(data)) // se guarda la configuracion en la sessionStorage
     $.ajax({
-        url: "/post_config",
+        url: "/post_config", //se fija la url de destino para enviar la configuracion
         type: 'POST',
-        data: JSON.stringify(data),
+        data: JSON.stringify(data), //se convierte a string el diccionario que guarda la configuracion actual
         dataType: "json",
         contentType: 'application/json',
         success: function(res) {
-            console.log(res);
+            console.log(res); //se muestra por consola la respuesta del server
 
         }
     })
 }
 
 
-function populate_tones(){
-    $( "#song_select" ).empty();
-    $.each(data['tonos_alarma'], function (i, p) {
-        console.log(i, p)
-        $('#song_select').append($('<option></option>').val(p).html(p));
+function populate_tones(){ //funcion que agrega los tonos disponibles al select
+    $( "#song_select" ).empty(); //se vacia en caso de que tenga algo
+    $.each(data['tonos_alarma'], function (i, p) { //se recorre el array que contiene los nombres
+        $('#song_select').append($('<option></option>').val(p).html(p)); //se agregan al select
 
     });
-    //$("#song_select[value='"+data['tono_alarma']+"']").attr("selected", true);
-    $('[name=options] option').filter(function() {
-            return ($(this).text() == data['tono_alarma']); //To select Blue
+    
+    $('[name=options] option').filter(function() { //se selecciona el tono que esta configurado
+            return ($(this).text() == data['tono_alarma']); //se cambia el valor por defecto del select
         }).prop('selected', true);
+
 }
 
 
-function show_loading(){
-    $('#close_song_modal').trigger('click');
-    $('#loading_modal').modal('show');
+function show_loading(){ //Se muestra un modal que contiene un gif de "cargando"
+    $('#close_song_modal').trigger('click');// cierra el modal actual 
+    $('#loading_modal').modal('show'); //abre el modal de carga
 }
 
 
-function hide_loading(){
-    setTimeout(function () {
-        $('#loading_modal').modal('hide');
-        data['tono_alarma'] = new_audio;
-        data['tonos_alarma'].push(new_audio);
-        document.getElementById("tone").innerHTML = new_audio;
-        $('#file').val('');
-        $( "#song_select" ).prop( "disabled", false );
-        post_data();
+function hide_loading(){  //funcion que esconde el modal de carga una vez que se subio el archivo
+    setTimeout(function () { //se esperan 3 segundos para que todo cargue correctamente
+        $('#loading_modal').modal('hide'); //se esconde el modal de carga
+        data['tono_alarma'] = new_audio; //se selecciona el tono de alarma que se acaba de subir
+        data['tonos_alarma'].push(new_audio); //se agrega al array que guarda los nombres de los tonos disponibles
+        document.getElementById("tone").innerHTML = new_audio; // se cambia el texto con la cancion seleccionada
+        $('#file').val(''); // se limpia el buffer de subida de video del modal de tonos 
+        $( "#song_select" ).prop( "disabled", false ); //se activa la casilla de seleccion de tono del modal
+        post_data(); //se envia la configuracion al servidor
     }, 3000);
+
+
 }
 
 
-function upload_file(){
-   var form_data = new FormData($('#upload-file')[0]);
+function upload_file(){ //funcion que sube los videos al servidor
+   var form_data = new FormData($('#upload-file')[0]); //se crea un formulario con el componente que contiene el archivo
 
-   show_loading();
-    $.ajax({
+   show_loading(); //se muestra el modal de carga
+    $.ajax({ //se envia a traves de un POST el archivo ala servidor
         type: 'POST',
-        url: '/post_file',
-        data: form_data,
+        url: '/post_file',  //se define la url donde van los archivos
+        data: form_data, //se adjunta el archivo
         contentType: false,
         cache: false,
         processData: false,
-        success: function(data) {
-            console.log("success!")
-            hide_loading();
+        success: function(data) { //una vez enviado el archivo 
+            console.log("success!"); //se muestra este mensaje por consola
+            hide_loading(); //se llama la funcion que esconde el modal
         },
     });
 }
